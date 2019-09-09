ec66f0e088e9a9631d2cbe9f5e288d9df4cd159d
Ercan Erdoğan <ercan.erdogan@innovile.com>
2019-09-09 11:16:06 +0300

    implementing Dependency Injection Approach

---
+++ DependencyInjection/IVehicle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DependencyInjection
+{
+    interface IVehicle
+    {
+        string Accelerate();
+        void Break();
+        void leftSignal();
+        void rightSignal();
+        void Shift();
+
+    }
+}
---
+++ DependencyInjection/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DependencyInjection
+{
+    class Program
+    {
+        class Car : IVehicle
+        {
+            public string Accelerate()
+            {
+                return "Driving a car";
+            }
+
+            public void Break()
+            {
+            }
+
+            public void leftSignal()
+            {
+            }
+
+            public void rightSignal()
+            {
+            }
+
+            public void Shift()
+            {
+            }
+        }
+
+        class Bus : IVehicle
+        {
+            public string Accelerate()
+            {
+                return "Driving a Bus";
+            }
+
+            public void Break()
+            {
+            }
+
+            public void leftSignal()
+            {
+            }
+
+            public void rightSignal()
+            {
+            }
+
+            public void Shift()
+            {
+            }
+
+       
+        }
+
+        class MotorCycle : IVehicle
+        {
+            public string Accelerate()
+            {
+                return "Driving a Motor Cycle like crazy..";
+            }
+
+            public void Break()
+            {
+            }
+
+            public void leftSignal()
+            {
+            }
+
+            public void rightSignal()
+            {
+            }
+
+            public void Shift()
+            {
+            }
+  
+        }
+
+        class Vehicle
+        {
+            IVehicle _vehicle;
+            public Vehicle(IVehicle vehicle)
+            {
+                _vehicle = vehicle;
+            }
+
+            public string Drive()
+            {
+                string result = null;
+                result = _vehicle.Accelerate();
+                _vehicle.Break();
+                _vehicle.leftSignal();
+                _vehicle.rightSignal();
+                _vehicle.Shift();
+
+                return result;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string result;
+            Vehicle vehicleCar = new Vehicle(new Car());
+            result = vehicleCar.Drive();
+            Console.WriteLine(result);
+
+            Vehicle vehicleBus = new Vehicle(new Bus());
+            result = vehicleBus.Drive();
+            Console.WriteLine(result);
+
+            Vehicle vehicleMotorCycle = new Vehicle(new MotorCycle());
+            result = vehicleMotorCycle.Drive();
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+
+        }
+    }
+}
